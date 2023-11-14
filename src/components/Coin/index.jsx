3922663893bbf4581182795d9a069a19ede706ec
mr-louback <loubacked4work@outlook.com>
2023-11-14 20:43:41 -0300

    input radio

--- src/components/Coin/index.jsx
+++ src/components/Coin/index.jsx
@@ -1,58 +1,102 @@
-import api from '../../services/api'
 import { useEffect, useState } from 'react'
+import api from '../../services/api'
 import './style.css'
+
 
 function Coin() {
   const [valueDollar, setValueDollar] = useState(0)
+  const [valueBitcoin, setValueBitcoin] = useState(0)
   const [valueTobeConverted, setValueTobeConverted] = useState(0)
-  const [valueConverted, setValueConverted] = useState(0)
+  let [valueConverted, setValueConverted] = useState(0)
+  const [selectedCurrency, setSelectedCurrency] = useState(localStorage.getItem('@value-current-name'))
 
   useEffect(() => {
     async function loadDollar() {
       const res = await api.get()
-      const data = Number(res.data.USDBRL.bid).toFixed(2)
-      setValueDollar(data)
+      const datadollar = Number(res.data.USDBRL.bid).toFixed(2)
+      const databitcoin = Number(res.data.BTCBRL.bid).toFixed(2)
+      setValueDollar(datadollar)
+      setValueBitcoin(databitcoin)
     }
     loadDollar()
   }, [])
-
+  const handleRadioCurrency = (e) => {
+    setSelectedCurrency(e.target.id)
+  }
   function handleClick() {
     if (valueTobeConverted > 0) {
-      let newValue = (valueTobeConverted / valueDollar).toFixed(2)
-      localStorage.setItem('@value-converted-to-be', valueTobeConverted.toString())
-      setValueTobeConverted(localStorage.getItem('@value-converted-to-be'))
-
+      let newValue = 0
+      switch (selectedCurrency) {
+        case 'bitcoin':
+          newValue = (valueBitcoin / valueTobeConverted).toFixed(1)
+          break;
+        case 'dolar':
+          newValue = (valueTobeConverted / valueDollar).toFixed(2)
+          break;
+        default:
+          alert('Você precisa selecionar uma moeda!')
+          break;
+      }
       localStorage.setItem('@value-converted', newValue.toString())
-      setValueConverted(localStorage.getItem('@value-converted'))
-    } else {
-      alert('Valor não pode ser zero ou menor que zero!')
+      localStorage.setItem('@value-converted-to-be', valueTobeConverted.toString())
+      localStorage.setItem('@value-current-name', selectedCurrency)
+
+    } else if (valueConverted <= 0) {
+      alert('Você precisa fornecer um valor maior que zero!')
     }
   }
   function handleChange(e) {
-    e.preventDefault()
     setValueTobeConverted(e.target.value)
   }
   return (
-    <div style={{ backgroundColor: 'blue', margin: '0 15px', borderRadius: '10px', padding: '10px'}}>
-      <form className='form'   >
+    <div className='form-coin'>
+      <form className='form'>
         <h2 htmlFor="valorCotacao">Valor atual do dólar é de R$ {valueDollar}</h2>
+
+        Escolha a moeda para qual moeda voce deseja converter
+        <div className='inputCheckBoxContainer'>
+          <label htmlFor="bitcoin">
+            Real para Bitcoin
+            <input
+              type="radio"
+              name="bitcoin"
+              id="bitcoin"
+              checked={selectedCurrency === 'bitcoin'}
+              onChange={handleRadioCurrency}
+              defaultChecked
+            />
+          </label>
+          <label htmlFor="dolar">
+            <input
+              type="radio"
+              name="dolar"
+              id="dolar"
+              checked={selectedCurrency === 'dolar'}
+              onChange={handleRadioCurrency}
+            />
+            Real para Dolar
+          </label>
+        </div>
         <span htmlFor="valorEmReal">Por favor digite o valor em reais a ser convertido</span>
+
         <input
           type='number'
-          onChange={e => handleChange(e)}
+          onChange={handleChange}
           value={valueTobeConverted}
           name='valorEmReal'
           placeholder='Valor a ser convertido'
         />
         <input type="submit" onClick={handleClick} value="Converter" />
+
       </form>
-      <div>
-        {`
-        R$ ${localStorage.getItem('@value-converted-to-be') == null ? 0 : localStorage.getItem('@value-converted-to-be')} em reais é $${localStorage.getItem('@value-converted') == null ? 0 : localStorage.getItem('@value-converted')} dólares.
-        `}
+      <div className='result-convertion'>
+        {
+          `R$ ${localStorage.getItem('@value-converted-to-be') ?? '0,00'} em ${localStorage.getItem('@value-current-name') ?? 'moeda'} é $${localStorage.getItem('@value-converted') ?? '0,00'}`
+        }
+
       </div>
     </div>
   )
 }
 
-export default Coin 
+export default Coin
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: `https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL`
+  baseURL: `https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL`
 })
 export default api

4eb62c641395d01c6bbfc292f3ed12ddc56e5af4
mr-louback <loubacked4work@outlook.com>
2023-11-14 22:31:02 -0300

    form

--- src/components/Distance/index.jsx
+++ src/components/Distance/index.jsx
@@ -1,14 +1,20 @@
 import './style.css'
 
 function Distance() {
-  
+
   return (
     <>
-    <form >
-      <h1>Conversor de distancia à velocidade da luz</h1>
-    </form>
+      <form className='form'>
+        <h1>Conversor de distância</h1>
+        <div>Por determinado tempo à velocidade da lúz</div>
+        <div>você terá percorrido que distância?</div>
+        <input type="number" name="time" id="time" placeholder='Tempo em minutos' />
+        <input type="submit" value="Calcular" />
+        <div id='res'>
+          Resultado
+        </div>
+      </form>
     </>
   )
 }
-
 export default Distance 
